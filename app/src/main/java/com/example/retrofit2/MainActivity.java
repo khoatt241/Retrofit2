492942f92d235e287a22f6aa011b28e34eefb08a
khoatt241 <khoatt241@gmail.com>
2019-08-09 20:38:17 +0700

    1

--- app/src/main/java/com/example/retrofit2/MainActivity.java
+++ app/src/main/java/com/example/retrofit2/MainActivity.java
@@ -4,11 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Protocol;
+import retrofit2.Retrofit;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        OkHttpClient okHttpClient =  new OkHttpClient.Builder() //set up cho client, request chặt chẽ hơn
+                .readTimeout(60, TimeUnit.SECONDS)
+                .writeTimeout(60, TimeUnit.SECONDS)
+                .connectTimeout(60, TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true)
+                .protocols(Arrays.asList(Protocol.HTTP_1_1))
+                .build();
+
+        // 1 : Khởi tạo retrofit
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://172.16.1.138:80/") // đường dẫn gốc, địa chỉ ipv4
+                .client(okHttpClient)
+                .build();
+
+        // 2 : Khởi tạo interface request
+
+        // 3 : Nhận giá trị qua callback
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/retrofit2/Requestrepository.java
@@ -0,0 +1,4 @@
+package com.example.retrofit2;
+
+public interface Requestrepository {
+}
